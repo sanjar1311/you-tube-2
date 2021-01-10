892ee2525461665d504b62ef0a4fe7d6755346dd
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-10 23:41:06 +0500

    add localStorage to watchLater

--- js/main.js
+++ js/main.js
@@ -102,9 +102,9 @@ elMenuBtn.addEventListener('click', function() {
   elMain.classList.toggle('active');
 });
 
-var localStorages = JSON.parse(localStorage.getItem('arrey'))
+var localStorages = JSON.parse(localStorage.getItem('arrey'));
 if(localStorages) {
-  displayVideo(videos, elFisrstUserVideos);
+  displayVideo(localStorages, elFisrstUserVideos);
 }
 if(localStorage.getItem('subscImg') && localStorage.getItem('subscTitle')) {
   elFirstLineUser.src = localStorage.getItem('subscImg');
@@ -131,186 +131,26 @@ elFisrstUserVideos.addEventListener('click', function(evt) {
       }
     }
   }
+  localStorage.setItem('watchL', JSON.stringify(watchLater));
 });
+
+var watchLaterLS = JSON.parse(localStorage.getItem('watchL'));
+if(watchLaterLS){
+  displayVideo(watchLaterLS, elWatchLaterOutput);
+}
+if(localStorage.getItem('watchImg') && localStorage.getItem('watchTitle')) {
+  watchLaterImg.src = localStorage.getItem('watchImg');
+  watchLaterImg.textContent = localStorage.getItem('watchTitle');
+}
+
 
 elWatchLater.addEventListener('click', function() {
-  $_('.first-line__watch-later-img').src = 'img/watch-later.png';
-  $_('.first-line__watch-later-title').textContent = 'Watch later';
+  var watchLaterImg = $_('.first-line__watch-later-img');
+  watchLaterImg.src = 'img/watch-later.png';
+  var watchLaterTitle = $_('.first-line__watch-later-title');
+  watchLaterTitle.textContent = 'Watch later';
+  localStorage.setItem('watchImg', 'img/watch-later.png');
+  localStorage.setItem('watchTitle', 'Watch later');
+
   displayVideo(watchLater,elWatchLaterOutput);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var videos = [
-//   {
-//     title: 'Pirates of the Caribbean 5',
-//     day: '2 weeks ago',
-//     views: '1 view',
-//     duration: '12:05',
-//     author: 'Gussie Singleton',
-//     img: 'img/user-1_2.jpg',
-//     id: 1
-//   },
-//   {
-//     title: 'Pirates of the Caribbean',
-//     day: '14 days ago',
-//     views: '80k views',
-//     duration: '15:15',
-//     author: 'Gussie Singleton',
-//     img: 'img/user-1_1.webp',
-//     id: 0
-//   },
-//   {
-//     title: 'Pirates of the Caribbean 6 Trailer',
-//     day: '1 day ago',
-//     views: '8 views',
-//     duration: '20:00',
-//     author: 'Gussie Singleton',
-//     img: 'img/user-1_3.jpg',
-//     id: 2
-//   }
-// ];
-// var favourite = [];
-
-
-
-// var outputFunc = function(arrey) {
-
-//   elUserPic.classList.add('site-header__gussie');
-//   elFirstLineUser.textContent = 'Gussie Singleton';
-//   $_('.first-line__inner').classList.add('first-line__inner--active');
-
-//   var firstLineTemplate = $_('.first-line-template').content;
-//   var videoFragment = document.createDocumentFragment();
-
-//   elFisrstLineList.innerHTML = '';
-//   for(var i = 0; i < arrey.length; i++) {
-//     var firstLineItem = firstLineTemplate.cloneNode(true);
-//     var videoTitle = $_('.video__title', firstLineTemplate);
-//     videoTitle.textContent = arrey[i].title;
-//     videoTitle.setAttribute('class', 'video__title');
-//     var videoViews = $_('.video__views', firstLineTemplate);
-//     videoViews.textContent = arrey[i].views;
-//     videoViews.setAttribute('class', 'video__views');
-//     var videoTime = $_('.video__time', firstLineTemplate);
-//     videoTime.textContent = arrey[i].day;
-//     videoTime.setAttribute('class', 'video__time');
-//     var videoAuthor = $_('.video__author', firstLineTemplate);
-//     videoAuthor.textContent = arrey[i].author;
-//     videoAuthor.setAttribute('class', 'video__author');
-//     var videoDuration = $_('.video__duration', firstLineTemplate);
-//     videoDuration.textContent = arrey[i].duration;
-//     videoDuration.setAttribute('class', 'video__duration');
-//     var videoImg = $_('.video__img', firstLineTemplate);
-//     videoImg.setAttribute('src', arrey[i].img);
-//     var videoAddBtn = $_('.video__add', firstLineItem);
-//     // videoAddBtn.setAttribute('dataset', arrey[i].id)
-//     // videoAddBtn.dataset.id = ;
-
-//      videoFragment.appendChild(firstLineItem);
-//   }
-//   elFisrstLineList.appendChild(videoFragment);
-// }
-
-// /* ========== Videos Output ========== */
-
-// var videosL = JSON.parse(localStorage.getItem('arrey'));
-// if(videosL) {
-//   outputFunc(videos);
-// }
-
-// elGussie.addEventListener('click', function() {
-
-//   outputFunc(videos);
-
-//   localStorage.setItem('arrey', JSON.stringify(videos));
-// });
-
-
-// /* ======== Add Favourite ========= */
-// elFisrstLineList.addEventListener('click', function(evt) {
-
-//   var firstLineTemplate = $_('.first-line-template').content;
-//   var firstLine = firstLineTemplate.cloneNode(true);
-//   var videoAdd = $_('.video__add', firstLine);
-
-//   if(evt.target.matches('.video__add')){
-//     for(var i = 0; i < videos.length; i++) {
-//       if(videos[i].id === Number(evt.target.dataset.id)){
-//         var index = Number(evt.target.dataset.id);
-//         favourite.push(videos[index]);
-//       }
-//     }
-//   }
-//   console.log(favourite);
-
-// });
-
-
-//           /* ========= Output Favourites ========== */
-// elWatchLater.addEventListener('click', function(){
-
-//   var elFavouriteSection = $_('.favourites');
-//   elFavouriteSection.classList.add('first-line__inner--favo');
-
-//   elFavouriteList.innerHTML = '';
-
-//   var favouriteFragment = document.createDocumentFragment();
-//   var firstLineTemplate = $_('.first-line-template').content;
-
-//   for(var i = 0; i < favourite.length; i++) {
-//     var favouriteItem = firstLineTemplate.cloneNode(true);
-
-//     favouriteFragment.appendChild(favouriteItem);
-//   }
-//   elFavouriteList.appendChild(favouriteFragment);
-
-// });
